4d895b220a611112db8b2c64f744130a030a2798
Marco Matthies <marco.matthies@gmail.com>
2022-09-23 21:07:45 +0200

    add all opt_md options

--- src/DssOpt.jl
+++ src/DssOpt.jl
@@ -9,9 +9,49 @@ import .LibDssOpt
 
 export opt_md
 
-function opt_md(target::AbstractString)
-    cmd = `$(DssOpt_jll.opt_md()) $target`
-    out = read(cmd, String)
+function opt_md(target::AbstractString;
+                seq_constraints_hard::Union{Nothing,AbstractString}=nothing,
+                timestep::Union{Nothing,Real}=nothing,
+                start_temperature::Union{Nothing,Real}=nothing,
+                time_total::Union{Nothing,Real}=nothing,
+                time_cool::Union{Nothing,Real}=nothing,
+                kpi::Union{Nothing,Real}=nothing,
+                kpa::Union{Nothing,Real}=nothing,
+                kneg::Union{Nothing,Real}=nothing,
+                khet::Union{Nothing,Real}=nothing,
+                het_window::Union{Nothing,Integer}=nothing,
+                cooling_type::Union{Nothing,Symbol}=nothing,
+                time_pur::Union{Nothing,Real}=nothing,
+                kpur_end::Union{Nothing,Real}=nothing,
+                seed::Union{Nothing,Integer}=nothing)
+    # unsupported command-line arguments:
+    # --time-print
+    # --movie
+    isempty(target) && return ""
+    cmd = `$(DssOpt_jll.opt_md())`
+    # TODO: check length(seq_constraints_hard) == length(target)
+    !isnothing(seq_constraints_hard) && (cmd = `$cmd --seq-constraints-hard $seq_constraints_hard`)
+    !isnothing(timestep) && (cmd = `$cmd --timestep $(float(timestep))`)
+    !isnothing(start_temperature) && (cmd = `$cmd --T-start $(float(start_temperature))`)
+    !isnothing(time_total) && (cmd = `$cmd --time-total $(float(time_total))`)
+    !isnothing(time_cool) && (cmd = `$cmd --time-cool $(float(time_cool))`)
+    !isnothing(kpi) && (cmd = `$cmd --kpi $(float(kpi))`)
+    !isnothing(kpa) && (cmd = `$cmd --kpa $(float(kpa))`)
+    !isnothing(kneg) && (cmd = `$cmd --kneg $(float(kneg))`)
+    !isnothing(khet) && (cmd = `$cmd --khet $(float(khet))`)
+    !isnothing(het_window) && (cmd = `$cmd --het-window $(Int(het_window))`)
+    if !isnothing(cooling_type)
+        if cooling_type ∈ (:linear, :exponential)
+            cmd = `$cmd --cooling-type $cooling_type`
+        else
+            throw(ArgumentError("cooling_type has to be one of: (:linear, :exponential)"))
+        end
+    end
+    !isnothing(time_pur) && (cmd = `$cmd --time-pur $(float(time_pur))`)
+    !isnothing(kpur_end) && (cmd = `$cmd --kpur-end $(float(kpur_end))`)
+    !isnothing(seed) && (cmd = `$cmd --seed $(Int(seed))`)
+
+    out = read(`$cmd $target`, String)
     seq = split(out, "\n")[end-1] |> s -> split(s, "=")[end] |> lstrip |> String
     return seq
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,26 @@ using Test
 using DssOpt
 
 @testset "opt_md" begin
-    @test opt_md("(((...)))") isa String
+    target = "(((...)))"
+    constr = "GNCANNNNC"
+    for kwargs in [
+        (; ),
+        (; seq_constraints_hard = constr),
+        (; timestep = 0.0001),
+        (; start_temperature = 10.0),
+        (; time_total = 5.0),
+        (; time_cool = 2.0),
+        (; kpi = 10000.1),
+        (; kpa = 10000.1),
+        (; kneg = 1.1),
+        (; khet = 10.1),
+        (; het_window = 3),
+        (; cooling_type = :linear),
+        (; cooling_type = :exponential),
+        (; time_pur = 2.0),
+        (; kpur_end = 1.1),
+        (; seed = 42),
+        ]
+        @test opt_md(target; kwargs...) isa String
+    end
 end
