1de11ece9057773745fa940e83799eb61fbb2376
Marco Matthies <marco.matthies@gmail.com>
2022-12-01 04:14:00 +0100

    improve tests: all in one outer testset, show test progress

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,90 +1,102 @@
 using Test
 using DssOpt
 
-@testset "opt_md" begin
-    target = "(((...)))"
-    constr = "GNCANNNNC"
-    for kwargs in [
-        (; ),
-        (; seq_constraints_hard = constr),
-        (; timestep = 0.0001),
-        (; start_temperature = 10.0),
-        (; time_total = 5.0),
-        (; time_cool = 2.0),
-        (; kpi = 10000.1),
-        (; kpa = 10000.1),
-        (; kneg = 1.1),
-        (; khet = 10.1),
-        (; het_window = 3),
-        (; cooling_type = :linear),
-        (; cooling_type = :exponential),
-        (; time_pur = 2.0),
-        (; kpur_end = 1.1),
-        (; seed = 42),
-        ]
-        s = opt_md(target; kwargs...)
-        @test s isa String
-        @test length(s) == length(target)
+# show which testset is currently running
+showtestset() = println(" "^(2 * Test.get_testset_depth()), "testing ",
+                        Test.get_testset().description)
+
+@testset verbose=true "DssOpt" begin
+    showtestset()
+
+    @testset "opt_md" begin
+        showtestset()
+        target = "(((...)))"
+        constr = "GNCANNNNC"
+        for kwargs in [
+            (; ),
+            (; seq_constraints_hard = constr),
+            (; timestep = 0.0001),
+            (; start_temperature = 10.0),
+            (; time_total = 5.0),
+            (; time_cool = 2.0),
+            (; kpi = 10000.1),
+            (; kpa = 10000.1),
+            (; kneg = 1.1),
+            (; khet = 10.1),
+            (; het_window = 3),
+            (; cooling_type = :linear),
+            (; cooling_type = :exponential),
+            (; time_pur = 2.0),
+            (; kpur_end = 1.1),
+            (; seed = 42),
+            ]
+            s = opt_md(target; kwargs...)
+            @test s isa String
+            @test length(s) == length(target)
+        end
+
+        # verbose
+        redirect_stdio(stdout=devnull, stderr=devnull) do
+            s = opt_md(target; verbose=true)
+            @test s isa String
+            @test length(s) == length(target)
+        end
     end
 
-    # verbose
-    redirect_stdio(stdout=devnull, stderr=devnull) do
-        s = opt_md(target; verbose=true)
-        @test s isa String
-        @test length(s) == length(target)
-    end
-end
+    @testset "opt_sd" begin
+        showtestset()
+        target = "(((...)))"
+        for kwargs in [
+            (; ),
+            (; maxsteps = 100),
+            (; kpi = 10000.1),
+            (; kpa = 10000.1),
+            (; kneg = 1.1),
+            (; kpur = 1.1),
+            (; khet = 10.1),
+            (; het_window = 3),
+            (; wiggle = 0.01),
+            (; seed = 42),
+            ]
+            s = opt_sd(target; kwargs...)
+            @test s isa String
+            @test length(s) == length(target)
+        end
 
-@testset "opt_sd" begin
-    target = "(((...)))"
-    for kwargs in [
-        (; ),
-        (; maxsteps = 100),
-        (; kpi = 10000.1),
-        (; kpa = 10000.1),
-        (; kneg = 1.1),
-        (; kpur = 1.1),
-        (; khet = 10.1),
-        (; het_window = 3),
-        (; wiggle = 0.01),
-        (; seed = 42),
-        ]
-        s = opt_sd(target; kwargs...)
-        @test s isa String
-        @test length(s) == length(target)
-    end
-
-    # verbose
-    redirect_stdio(stdout=devnull, stderr=devnull) do
-        s = opt_sd(target; verbose=true)
-        @test s isa String
-        @test length(s) == length(target)
-    end
-end
-
-@testset "opt_sd_gsl" begin
-    target = "(((...)))"
-    for kwargs in [
-        (; ),
-        (; maxsteps = 100),
-        (; kpi = 10000.1),
-        (; kpa = 10000.1),
-        (; kneg = 1.1),
-        (; kpur = 1.1),
-        (; khet = 10.1),
-        (; het_window = 3),
-        (; wiggle = 0.01),
-        (; seed = 42),
-        ]
-        s = opt_sd_gsl(target; kwargs...)
-        @test s isa String
-        @test length(s) == length(target)
+        # verbose
+        redirect_stdio(stdout=devnull, stderr=devnull) do
+            s = opt_sd(target; verbose=true)
+            @test s isa String
+            @test length(s) == length(target)
+        end
     end
 
-    # verbose
-    redirect_stdio(stdout=devnull, stderr=devnull) do
-        s = opt_sd_gsl(target; verbose=true)
-        @test s isa String
-        @test length(s) == length(target)
+    @testset "opt_sd_gsl" begin
+        showtestset()
+        target = "(((...)))"
+        for kwargs in [
+            (; ),
+            (; maxsteps = 100),
+            (; kpi = 10000.1),
+            (; kpa = 10000.1),
+            (; kneg = 1.1),
+            (; kpur = 1.1),
+            (; khet = 10.1),
+            (; het_window = 3),
+            (; wiggle = 0.01),
+            (; seed = 42),
+            ]
+            s = opt_sd_gsl(target; kwargs...)
+            @test s isa String
+            @test length(s) == length(target)
+        end
+
+        # verbose
+        redirect_stdio(stdout=devnull, stderr=devnull) do
+            s = opt_sd_gsl(target; verbose=true)
+            @test s isa String
+            @test length(s) == length(target)
+        end
     end
+
 end
